e87448c1c0a7d736873c2a337cff8ec4d4ea22c9
daniw <dani.winz@bluewin.ch>
2013-09-08 23:39:45 +0200

    Datenblätter der Molex Steckverbinder

---
+++ datasheets/datasheets.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# drawing molex 47300
+wget http://www.molex.com/elqNow/elqRedir.htm?ref=http://www.molex.com/pdm_docs/sd/473001020_sd.pdf
+
+# drawing molex 67492
+wget http://www.molex.com/elqNow/elqRedir.htm?ref=http://www.molex.com/pdm_docs/sd/674929222_sd.pdf
+
+
